72b32a2ea45408c4c004e7095a5f5203b758671b
Alex <alexdavidhurtado@gmail.com>
2023-01-05 14:36:16 -0500

    Register. Plantilla del correo de cofirmación

--- resources/views/livewire/register/confirmation_code.blade.php
+++ resources/views/livewire/register/confirmation_code.blade.php
@@ -1,21 +1,55 @@
-<html lang="es">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    {{-- Necesario para el funcionamiento de laviwire --}}
-    @livewireStyles
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>confirmation_code</title>
+
+    <style>
+
+        /* Utility */
+        .a-i-center{
+            align-items: center
+        }
+        
+        .c-blue{
+        color: rgb(22, 54, 131);
+        }
+        .d-flex{
+            display: flex
+        }
+        .f-colum{
+            flex-direction: column;
+        }
+        .j-c-center{
+            justify-content: center
+        }
+        .t-none{
+            text-decoration: none;
+        }
+
+       
+
+
+    </style>
 </head>
 <body>
-    <h2 class="c-blue">Hi {{ $Name }}, thank you for register in <strong> Besana</strong> !</h2>
-    <p>Please, confirm your email</p>
- 
 
-    <a href="{{ url('/register/verify/' . $confirmation_code) }}">
-        Click for confirm your email
-    </a>
+    <div class="d-flex f-colum j-c-center a-i-center">
 
+        <div>
+            <img src="{{asset('img/logonew.png')}}" alt="logo Besana" height="100">
+        </div>
 
-{{-- Necesario para el funcionamiento de laviwire --}}
-    @livewireScripts
+        <h2 class="c-blue">Hi {{$Name}}, thank you for register in <strong> Besana</strong> !</h2>
+        <p>Please, confirm your email</p>
+     
+    
+        <a class="t-none" href="{{ url('/register/verify/' . $confirmation_code) }}">
+            Click for confirm your email
+        </a>
+    </div>
+    
 </body>
 </html>
