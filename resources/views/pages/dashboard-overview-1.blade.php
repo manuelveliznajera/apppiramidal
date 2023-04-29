557a32c8281fe4fa7c23ae96bce29c7e5aaba89f
Manuel de Jesus Veliz Najera <manueldejesusveliznajera@MacBook-Pro-de-Joksan.local>
2023-04-28 20:34:15 -0600

    wallet week wallet month

--- resources/views/pages/dashboard-overview-1.blade.php
+++ resources/views/pages/dashboard-overview-1.blade.php
@@ -120,7 +120,7 @@
                                                                 @if (count($walletWeek)>0)
                                                                     <td class="border text-center py-2">  <span class="font-bold text-orange-600  ">$ {{round($walletWeek[0]->total, 2)}}</span></td>
                                                                     <td class="border text-center p-2">                                                    
-                                                                        @if ($walletMonth[0]->estado=='solicitado'||$walletMonth[0]->estado=='pendiente')
+                                                                        @if ($walletMonth[0]->estado=='pendiente')
                                                                             <span class="text-lg font-extrabold">{{__('Request')}}</span>  
                                                                         @else
                                                                             <form action="{{route('solicitaWeek')}}" method="POST" >
